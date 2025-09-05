d3b10534dd0681b46bcaae3604ac32f20ad4f361
Никита <nicki3a@mail.ru>
2025-09-05 13:37:35 +0300

    Homework12

---
+++ Author.java
@@ -0,0 +1,19 @@
+public class Author {
+    private String fullName;
+
+    public Author(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String toString() {
+        return fullName;
+    }
+}
